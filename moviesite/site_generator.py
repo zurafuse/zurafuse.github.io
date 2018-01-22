dec97912f1fef7242316eac8ec889e994b5a8ea6
Timothy Horton <zurafuse@gmail.com>
2018-01-22 17:22:36 -0600

    Add files via upload

--- moviesite/site_generator.py
+++ moviesite/site_generator.py
@@ -120,7 +120,7 @@ main_page_content = '''
 
 # A single movie entry html template
 movie_tile_content = '''
-<div class="col-md-6 movie-tile text-center" data-youtube-embed='{youtube_embed_url}' data-trailer-youtube-id="{trailer_youtube_id}" data-toggle="modal" data-target="#trailer">
+<div class="col-md-6 col-lg-4 movie-tile text-center" data-youtube-embed='{youtube_embed_url}' data-trailer-youtube-id="{trailer_youtube_id}" data-toggle="modal" data-target="#trailer">
     <img src="{poster_image_url}" width="220" height="342">
     <h2>{movie_title}</h2>
 </div>
