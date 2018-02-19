5fefc38f4291ef90e901b574e7cca29d370b5134
Timothy Horton <zurafuse@gmail.com>
2018-02-19 13:52:18 -0600

    Add files via upload

--- ExcellentSpirit/js/app.js
+++ ExcellentSpirit/js/app.js
@@ -1,1 +1,17 @@
+//perform an update to see if any trophies should be automatically assigned.
+for (i = 0; i < ES_Trophies.length; i++)
+{
+	ES_Trophies[i].update();
+}
 
+
+$("body").append("<h2 class='title'>PLEASE SELECT A PLAYER.</h2><br><div class='container'><div class='row players'>");
+
+
+for (i in ESuserArray)
+{
+	$(".players").append("<div class='col-md avatar-name'><h3>" + ESuserArray[i].name +
+	"</h3>" + "<h5>" + ESuserArray[i].nickName + "</h5>" + "<img src='" + ESuserArray[i].img + "' alt='Avatar'/><br></div>");	
+}
+
+$("body").append("</div></div>");
