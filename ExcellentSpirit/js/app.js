ab64cd16671bee6bb1f3d26ac24a62cc0b69e686
Timothy Horton <zurafuse@gmail.com>
2018-02-19 21:09:16 -0600

    Add files via upload

--- ExcellentSpirit/js/app.js
+++ ExcellentSpirit/js/app.js
@@ -5,13 +5,13 @@ for (i = 0; i < ES_Trophies.length; i++)
 }
 
 
-$("body").append("<h2 class='title'>PLEASE SELECT A PLAYER.</h2><br><div class='container'><div class='row players'>");
+$("body").append("<h2 class='title'>PLEASE SELECT A PLAYER.</h2><br><div class='container-fluid'><div class='row players'>");
 
 
 for (i in ESuserArray)
 {
-	$(".players").append("<div class='col-md avatar-name' id='user" + ESuserArray[i].id + "'><h3>" + ESuserArray[i].name +
-	"</h3>" + "<h5>" + ESuserArray[i].nickName + "</h5>" + "<img src='" + ESuserArray[i].img + "' alt='Avatar'/><br></div>");	
+	$(".players").append("<div class='col-sm avatar-name' id='user" + ESuserArray[i].id + "'><h3>" + ESuserArray[i].name +
+	"</h3>" + "<h5>" + ESuserArray[i].nickName + "</h5>" + "<img src='" + ESuserArray[i].img + "' alt='Avatar' class='img-responsive'/><br></div>");	
 }
 
 $("body").append("</div></div>");
--- ExcellentSpirit/js/controller.js
+++ ExcellentSpirit/js/controller.js
@@ -12,17 +12,31 @@ var viewStatPage = function(user){
 	}
 	$(".title").remove();
 	$(".players").remove();
-	$(".container").append("<div class='row'><div class='col-md'><div class='row'><p>" + thisUser.name + 
+	$(".container-fluid").append("<div class='row'><div class='col-sm-6'><div class='row'><p>" + thisUser.name + 
 		" (" + thisUser.nickName + ")</p></div><div class='row'><p>Rank: " + thisUser.title + "</p></div>" + 
 		"<div class='row'><p>Level: " + thisUser.level + "</p></div>" + 
 		"<div class='row'><p>Experience Points: " + thisUser.xp + "</p></div>" + 
 		"<div class='row'><p>Coins: " + thisUser.money + "</p></div></div>" + 
-		"<div class='col-md'><img src='" + thisUser.img + "' alt='avatar'></div></div>" + 
-		"<div class='row'><div class='col-md'><div class='row'><p>Health: " + 
+		"<div class='col-sm-6'><img src='" + thisUser.img + "' alt='avatar' class='img-responsive'></div></div>" + 
+		"<div class='row'><div class='col-sm-6'><div class='row'><p>Health: " + 
 		thisUser.health + "</p></div><div class='row'><p>Stars: " + thisUser.stars + 
-		"</p></div></div><div class='col-md'><div class='row'><p>Status: " + thisUser.stat + 
-		"</p></div><div class='row'><p>Demerits: " + thisUser.demerits + "</p></div></div></div>" + 
-		"<div class='row'><p>Trophies: </p></div></div>");
+		"</p></div></div><div class='col-sm-6'><div class='row'><p>Status: " + thisUser.stat + 
+		"</p></div><div class='row'><p>Demerits: " + thisUser.demerits + "</p></div></div></div>");
+	if (thisUser.trophies.length > 0)
+	{
+		$(".container-fluid").append("<div class='row'><p>Trophies:</p></div>");
+		for (i = 0; i < thisUser.trophies.length; i++)
+		{
+			for (j = 0; j < EStrophyArray.length; j++)
+			{
+				if (thisUser.trophies[i] == EStrophyArray[j].id)
+				{
+					$(".container-fluid").append("<div class='row'><img src='" + 
+					EStrophyArray[j].img + "' alt='trophy' /></div>");
+				}
+			}
+		}
+	}
 };	
 
 
