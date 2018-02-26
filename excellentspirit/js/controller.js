f4e5c1c29aef7335de407a933a7a375ad6dd65aa
Timothy Horton <zurafuse@gmail.com>
2018-02-26 09:40:42 -0600

    Update controller.js

--- excellentspirit/js/controller.js
+++ excellentspirit/js/controller.js
@@ -78,10 +78,10 @@ var viewStatPage = function(user){
 		"<div class='row'><p>Level: " + thisUser.level + "</p></div>" + "<div class='row'><p>Experience Points: " + 
 		thisUser.xp + "</p></div>" + "<div class='row'><p>Coins: " + thisUser.money + "</p></div></div></div>" + 
 		"<div class='row'><div class='col-sm-4'><div class='row'><p>Health: " + thisUser.health + "</p></div><div class='row'><p>Stars: " + 
-		thisUser.stars + "</p></div><div class='row user-prizes'>Prizes (" + thisUser.prizes.length + ")</div><div class='row user-quests'>Quests" + 
+		thisUser.stars + "</p></div><div class='row'><button user-prizes>Prizes (" + thisUser.prizes.length + ")</button></div><div class='row'><button user-quests>Quests</button>" + 
 		"</div></div><div class='col-sm-8'><div class='row'><p>Status: " + thisUser.stat + 
-		"</p></div><div class='row'><p>Demerits: " + thisUser.demerits + "</p></div><div class='row user-trophies'><p>Trophies (" + 
-		thisUser.trophies.length + ")</p></div></div></div>");
+		"</p></div><div class='row'><p>Demerits: " + thisUser.demerits + "</p></div><div class='row'><button user-trophies>Trophies (" + 
+		thisUser.trophies.length + ")</button></div></div></div>");
 		$(".user-trophies").click("click", function(){getTrophies(thisUser)});
 		$(".user-prizes").click("click", function(){getPrizes(thisUser)});
 		$(".user-quests").click("click", function(){getQuests(thisUser)});
