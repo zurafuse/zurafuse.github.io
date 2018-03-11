4773892c92ea1d59c034908afbdf489229071f71
Timothy Horton <zurafuse@gmail.com>
2018-03-10 21:53:53 -0600

    Add files via upload

--- moviesite/entertainment_center.py
+++ moviesite/entertainment_center.py
@@ -1,33 +1,82 @@
 import site_generator
 import media
 
-wonder_woman = media.Movie("Wonder Woman", "Description", "https://upload.wikimedia.org/wikipedia/en/e/ed/Wonder_Woman_%282017_film%29.jpg",
+wonder_woman = media.Movie("Wonder Woman", "Description", "images/wonder_woman.jpg",
                            "https://www.youtube.com/watch?v=VSB4wGIdDwo", '<iframe width="854" height="480" src="https://www.youtube.com/embed/VSB4wGIdDwo" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>')
 
-guardians_of_galaxy = media.Movie("Guardians of the Galaxy", "Description", "https://upload.wikimedia.org/wikipedia/en/thumb/8/8f/GOTG-poster.jpg/220px-GOTG-poster.jpg",
+guardians_of_galaxy = media.Movie("Guardians of the Galaxy", "Description", "images/guardians_of_the_galaxy.jpg",
                                   "https://www.youtube.com/watch?v=d96cjJhvlMA", '<iframe width="854" height="480" src="https://www.youtube.com/embed/d96cjJhvlMA" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>')
 
-guardians_of_galaxy2 = media.Movie("Guardians of the Galaxy Vol 2", "Description", "https://upload.wikimedia.org/wikipedia/en/thumb/9/95/GotG_Vol2_poster.jpg/220px-GotG_Vol2_poster.jpg",
+guardians_of_galaxy2 = media.Movie("Guardians of the Galaxy Vol 2", "Description", "images/guardians_of_the_galaxy2.jpg",
                                    "https://www.youtube.com/watch?v=2cv2ueYnKjg", '<iframe width="854" height="480" src="https://www.youtube.com/embed/2cv2ueYnKjg" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>')
 
-spiderman = media.Movie("Spider-Man: Homecoming", "2017 American superhero film based on the Marvel Comics character Spider-Man", "https://upload.wikimedia.org/wikipedia/en/thumb/f/f9/Spider-Man_Homecoming_poster.jpg/220px-Spider-Man_Homecoming_poster.jpg",
+spiderman = media.Movie("Spider-Man: Homecoming", "2017 American superhero film based on the Marvel Comics character Spider-Man", "images/spider-man.jpg",
                         "https://www.youtube.com/watch?v=oNE0zRNyXuc", '<iframe width="640" height="360" src="https://www.youtube.com/embed/oNE0zRNyXuc" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>')
 
 man_of_steel = media.Movie("Man of Steel", "2013 superhero film featuring the DC Comics character Superman", "images/man_of_steel.jpg",
                         "https://www.youtube.com/watch?v=T6DJcgm3wNY", '<iframe width="640" height="360" src="https://www.youtube.com/embed/T6DJcgm3wNY" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>')
 
-batman_vs_superman = media.Movie("Batman v Superman: Dawn of Justice", "2016 American superhero film featuring the DC Comics characters Batman and Superman", "https://upload.wikimedia.org/wikipedia/en/thumb/2/20/Batman_v_Superman_poster.jpg/220px-Batman_v_Superman_poster.jpg",
+batman_vs_superman = media.Movie("Batman v Superman: Dawn of Justice", "2016 American superhero film featuring the DC Comics characters Batman and Superman", "images/batman_superman.jpg",
                         "https://www.youtube.com/watch?v=fis-9Zqu2Ro", '<iframe width="854" height="480" src="https://www.youtube.com/embed/fis-9Zqu2Ro" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>')
 
-justice_league = media.Movie("Justice League", "2017 American superhero film based on the DC Comics superhero team", "https://upload.wikimedia.org/wikipedia/en/3/31/Justice_League_film_poster.jpg",
+justice_league = media.Movie("Justice League", "2017 American superhero film based on the DC Comics superhero team", "images/justice_league.jpg",
                         "https://www.youtube.com/watch?v=r9-DM9uBtVI", '<iframe width="640" height="360" src="https://www.youtube.com/embed/r9-DM9uBtVI" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>')
 
-dbz_rezF = media.Movie("Dragon Ball Z: Resurrection F", "Description", "https://upload.wikimedia.org/wikipedia/en/thumb/6/6a/DBZ_THE_MOVIE_NO._15.png/220px-DBZ_THE_MOVIE_NO._15.png",
+dbz_rezF = media.Movie("Dragon Ball Z: Resurrection F", "Description", "images/dbz_resurrection_f.png",
                         "https://www.youtube.com/watch?v=WiONylGn8Xw", '<iframe width="854" height="480" src="https://www.youtube.com/embed/WiONylGn8Xw" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>')
 
-incredibles2 = media.Movie("The Incredibles 2", "2nd installment of the Pixar super hero film.", "https://upload.wikimedia.org/wikipedia/en/2/27/The_Incredibles_2.jpg",
+#incredibles = media.Movie("The Incredibles", "A Pixar super hero film.", "https://upload.wikimedia.org/wikipedia/en/thumb/e/ec/The_Incredibles.jpg/220px-The_Incredibles.jpg",
+#                        "https://www.youtube.com/watch?v=eZbzbC9285I", '<iframe width="640" height="360" src="https://www.youtube.com/embed/eZbzbC9285I" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>')	
+
+incredibles2 = media.Movie("The Incredibles 2", "2nd installment of the Pixar super hero film.", "images/The_Incredibles_2.jpg",
                         "https://www.youtube.com/watch?v=2_VhcvodRBY", '<iframe width="640" height="360" src="https://www.youtube.com/embed/2_VhcvodRBY" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>')					
 						
+#toy_story = media.Movie("Toy Story", "A movie about a boy and his toys which come to life when he is not looking.",
+#                        "https://upload.wikimedia.org/wikipedia/en/thumb/1/13/Toy_Story.jpg/220px-Toy_Story.jpg",
+#                        "https://www.youtube.com/watch?v=KYz2wyBy3kc", '<iframe width="854" height="480" src="https://www.youtube.com/embed/KYz2wyBy3kc" frameborder="0" allowfullscreen></iframe>')
+
+#toy_story2 = media.Movie("Toy Story 2", "Woody is stolen by a toy collector, prompting Buzz Lightyear and his friends to vow to rescue him.", "https://upload.wikimedia.org/wikipedia/en/thumb/c/c0/Toy_Story_2.jpg/220px-Toy_Story_2.jpg",
+#                        "https://www.youtube.com/watch?v=Lu0sotERXhI", '<iframe width="640" height="360" src="https://www.youtube.com/embed/Lu0sotERXhI" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>')
+
+#toy_story3 = media.Movie("Toy Story 3", "Woody, Buzz Lightyear, and their friends deal with an uncertain future as their owner, Andy, prepares to leave for college", "https://upload.wikimedia.org/wikipedia/en/6/69/Toy_Story_3_poster.jpg",
+#                        "https://www.youtube.com/watch?v=JcpWXaA2qeg", '<iframe width="640" height="360" src="https://www.youtube.com/embed/JcpWXaA2qeg" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>')
+
+#avatar = media.Movie("Avatar", "A movie about a bunch of marines stranded on an alien planet.",
+#                       "https://upload.wikimedia.org/wikipedia/en/thumb/b/b0/Avatar-Teaser-Poster.jpg/220px-Avatar-Teaser-Poster.jpg",
+#                        "https://www.youtube.com/watch?v=d1_JBMrrYw8", '<iframe width="854" height="480" src="https://www.youtube.com/embed/d1_JBMrrYw8" frameborder="0" allowfullscreen></iframe>')
+
+#meatballs = media.Movie("Cloudy with a Chance of Meatballs", "Flint Lockwood, a young science enthusiast on an island called Swallow Falls, has developed a machine capable of materializing everyone's favorite food.",
+#                        "https://upload.wikimedia.org/wikipedia/en/thumb/d/d1/Cloudy_with_a_chance_of_meatballs_theataposter.jpg/220px-Cloudy_with_a_chance_of_meatballs_theataposter.jpg",
+#                        "https://www.youtube.com/watch?v=pUaKcFI4BZY", '<iframe width="854" height="480" src="https://www.youtube.com/embed/pUaKcFI4BZY" frameborder="0" allowfullscreen></iframe>')
+
+#meatballs2 = media.Movie("Cloudy with a Change of Meatballs 2", "Flint and his friends are forced to leave their home town, but when the food machine reawakens—this time producing sentient food beasts—they must return to save the world.", "https://upload.wikimedia.org/wikipedia/en/thumb/7/77/Cloudy_with_a_Chance_of_Meatballs_2.jpg/220px-Cloudy_with_a_Chance_of_Meatballs_2.jpg",
+#                        "https://www.youtube.com/watch?v=3QCJTbHU60U", '<iframe width="640" height="360" src="https://www.youtube.com/embed/3QCJTbHU60U" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>')
+
+#hunger_games = media.Movie("The Hunger Games", 'In a dystopian post-apocalyptic future in the nation of Panem, which is divided into 12 districts, where a boy and a girl from each district, between the ages of 12 and 18 must take part in The Hunger Games, a televised annual event in which the "tributes" of each district are required to fight to the death until there is only one survivor.', "https://upload.wikimedia.org/wikipedia/en/4/42/HungerGamesPoster.jpg",
+#                        "https://www.youtube.com/watch?v=mfmrPu43DF8", '<iframe width="640" height="360" src="https://www.youtube.com/embed/mfmrPu43DF8" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>')
+
+#hunger_games2 = media.Movie("The Hunger Games: Catching Fire", "Part 2 in the Hunger Games film series.", "https://upload.wikimedia.org/wikipedia/en/1/12/Catching-Fire_poster.jpg",
+#                        "https://www.youtube.com/watch?v=MkvUNfySGQU", '<iframe width="640" height="360" src="https://www.youtube.com/embed/MkvUNfySGQU" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>')
+                            
+#hunger_games3 = media.Movie("The Hunger Games: Mockingjay - Part 1", "Third movie of the Hunger Games film series.", "https://upload.wikimedia.org/wikipedia/en/thumb/6/63/MockingjayPart1Poster3.jpg/220px-MockingjayPart1Poster3.jpg",
+#                        "https://www.youtube.com/watch?v=C_Tsj_wTJkQ", '<iframe width="640" height="360" src="https://www.youtube.com/embed/C_Tsj_wTJkQ" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>')
+
+#hunger_games4 = media.Movie("The Hunger Games: Mockingjay - Part 2", "Fourth movie of the Hunger Games film series.", "https://upload.wikimedia.org/wikipedia/en/thumb/9/9d/Mockingjay_Part_2_Poster.jpg/220px-Mockingjay_Part_2_Poster.jpg",
+#                        "https://www.youtube.com/watch?v=n-7K_OjsDCQ", '<iframe width="640" height="360" src="https://www.youtube.com/embed/n-7K_OjsDCQ" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>')
+
+#war_room = media.Movie("War Room", "Description", "images/war_room.jpg",
+#                        "https://www.youtube.com/watch?v=2DbRwcrhiLA", '<iframe width="640" height="360" src="https://www.youtube.com/embed/2DbRwcrhiLA" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>')
+
+#star_wars_rogue = media.Movie("Rogue One", "Another Star Wars story.", "https://upload.wikimedia.org/wikipedia/en/thumb/d/d4/Rogue_One%2C_A_Star_Wars_Story_poster.png/220px-Rogue_One%2C_A_Star_Wars_Story_poster.png",
+#                       "https://www.youtube.com/watch?v=frdj1zb9sMY", '<iframe width="640" height="360" src="https://www.youtube.com/embed/frdj1zb9sMY" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>')
+
+#star_wars7 = media.Movie("Star Wars: The Force Awakens", "The Seventh movie in the Star Wars film series.", "https://upload.wikimedia.org/wikipedia/en/a/a2/Star_Wars_The_Force_Awakens_Theatrical_Poster.jpg",
+#                        "https://www.youtube.com/watch?v=sGbxmsDFVnE", '<iframe width="640" height="360" src="https://www.youtube.com/embed/sGbxmsDFVnE" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>')
+
+#star_wars8 = media.Movie("Star Wars: The Last Jedi", "The Eighth movie in the Star Wars film series.", "https://upload.wikimedia.org/wikipedia/en/7/7f/Star_Wars_The_Last_Jedi.jpg",
+#                        "https://www.youtube.com/watch?v=Q0CbN8sfihY", '<iframe width="640" height="360" src="https://www.youtube.com/embed/Q0CbN8sfihY" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>')
+
+
 
 movies = [wonder_woman, guardians_of_galaxy, guardians_of_galaxy2, spiderman, man_of_steel, batman_vs_superman, justice_league, dbz_rezF, incredibles2]
 
