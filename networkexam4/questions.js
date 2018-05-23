4429872fa07681e99bef5702f98aac177ef38148
Timothy Horton <zurafuse@gmail.com>
2018-05-22 19:37:00 -0500

    Add files via upload

---
+++ networkexam4/questions.js
@@ -0,0 +1,307 @@
+var examQuest = [];
+
+examQuest[0] = {
+question: "What does opening a port do?",
+choicea: "A. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;blocks information from flowing through the port",
+choiceb: "B. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;allows information to flow though the port",
+choicec: "C. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Connects the Application Layer with packets",
+choiced: "D. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;There's candy inside",
+answer: "B"
+};
+
+examQuest[1] = {
+question: "a Network firewall can be a separate appliance or on a router",
+choicea: "A. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;False",
+choiceb: "B. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;True",
+choicec: "C. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Maybe",
+choiced: "D. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Sometimes",
+answer: "B"
+};
+
+examQuest[2] = {
+question: "What are the main types of VPN?",
+choicea: "A. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;PPTP, L2TP, IKE",
+choiceb: "B. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;PPP, L2TP, IKE",
+choicec: "C. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;IP Tunnels and EAS",
+choiced: "D. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;CAT5e and CAT6",
+answer: "A"
+};
+
+examQuest[3] = {
+question: "What is the Class C IP address range?",
+choicea: "A. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;192.0.0.0-255.255.255.255",
+choiceb: "B. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;192.10.0.0-223.255.255.255",
+choicec: "C. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;10.0.0.0-223.255.255.255",
+choiced: "D. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;192.0.0.0-223.255.255.255",
+answer: "D"
+};
+
+examQuest[4] = {
+question: "What is the Class A IP address range?",
+choicea: "A. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;0.0.0.0-126.255.255.255",
+choiceb: "B. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;0.0.0.0-223.255.255.255",
+choicec: "C. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;192.0.0.0-126.255.255.255",
+choiced: "D. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;10.0.0.0-126.255.255.255",
+answer: "A"
+};
+
+examQuest[5] = {
+question: "What is the Class B IP Address Range?",
+choicea: "A. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choiceb: "B. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;0.0.0.0-126.255.255.255",
+choicec: "C. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;128.0.0.0-191.255.255.255",
+choiced: "D. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+answer: "C"
+};
+
+examQuest[6] = {
+question: "What is the Class D IP address range?",
+choicea: "A. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choiceb: "B. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choicec: "C. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;224.0.0.0-239.255.255.255",
+choiced: "D. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+answer: "C"
+};
+
+examQuest[7] = {
+question: "What is the Class E IP address range?",
+choicea: "A. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choiceb: "B. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choicec: "C. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choiced: "D. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;240.0.0.0-254.255.255.255",
+answer: "D"
+};
+
+examQuest[8] = {
+question: "What are SHA1, SHA256, MD5",
+choicea: "A. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;hash algorithm for data integrity",
+choiceb: "B. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choicec: "C. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choiced: "D. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+answer: "A"
+};
+
+examQuest[9] = {
+question: "What is passive and detects and warns a network administrator to take action?",
+choicea: "A. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choiceb: "B. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choicec: "C. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Intrusion Detection System (IDS)",
+choiced: "D. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+answer: "C"
+};
+
+examQuest[10] = {
+question: "What is active and will take action to protect real time?",
+choicea: "A. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choiceb: "B. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Intrusion Prevention System (IPS).",
+choicec: "C. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choiced: "D. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+answer: "B"
+};
+
+examQuest[11] = {
+question: "What is the color order for T568A?",
+choicea: "A. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choiceb: "B. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;green-white, green, orange-white, blue, blue-white, orange, brown-white, brown",
+choicec: "C. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choiced: "D. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;orange-white, orange, green-white, blue, blue-white, green, brown-white, brown",
+answer: "B"
+};
+
+examQuest[12] = {
+question: "What is the color order for T568B?",
+choicea: "A. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choiceb: "B. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;green-white, green, orange-white, blue, blue-white, orange, brown-white, brown",
+choicec: "C. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choiced: "D. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;orange-white, orange, green-white, blue, blue-white, green, brown-white, brown",
+answer: "D"
+};
+
+examQuest[13] = {
+question: "10Mbps Ethernet over fiber - generic specification for fiber",
+choicea: "A. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choiceb: "B. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choicec: "C. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;10BASE-F",
+choiced: "D. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+answer: "C"
+};
+
+examQuest[14] = {
+question: "10BASE-FB",
+choicea: "A. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choiceb: "B. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choicec: "C. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choiced: "D. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;10Mbps Ethernet over fiber - part of the IEEE 10BASE-F specificatoin; segments can be up to 2 km in length",
+answer: "D"
+};
+
+examQuest[15] = {
+question: "10 Mbps Ethernet over fiber - segments can be up to 2 km in length; it replaces the FOIRL specification.",
+choicea: "A. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;10BASE-FL",
+choiceb: "B. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choicec: "C. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choiced: "D. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+answer: "A"
+};
+
+examQuest[16] = {
+question: "10BASE-FP",
+choicea: "A. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choiceb: "B. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;A passive fiber star network; segments can be up to 500 m in length.",
+choicec: "C. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choiced: "D. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+answer: "B"
+};
+
+examQuest[17] = {
+question: "100BASE-FX",
+choicea: "A. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choiceb: "B. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choicec: "C. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;A 100Mbps fast Ethernet standard that uses two fiber strands.",
+choiced: "D. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+answer: "C"
+};
+
+examQuest[18] = {
+question: "Gigabit Ethernet standard that uses fiber strands using long wavelength transmitters",
+choicea: "A. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choiceb: "B. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choicec: "C. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;1000BASE-LX",
+choiced: "D. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+answer: "C"
+};
+
+examQuest[19] = {
+question: "Gigabit Ethernet standard using short-wavelength transmitters?",
+choicea: "A. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choiceb: "B. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choicec: "C. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choiced: "D. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;1000BASE-LX",
+answer: "D"
+};
+
+examQuest[20] = {
+question: "10GBASE-R",
+choicea: "A. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choiceb: "B. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;10 gigabit (10.325Gbps) Ethernet for LANs",
+choicec: "C. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choiced: "D. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+answer: "B"
+};
+
+examQuest[21] = {
+question: "10 gigabit (9.95328 Gbps) Ethernet for WANs using OC-192 and SONET Framing?",
+choicea: "A. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choiceb: "B. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choicec: "C. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choiced: "D. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;10GBASE-W",
+answer: "D"
+};
+
+examQuest[22] = {
+question: "CAT3 data rate?",
+choicea: "A. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choiceb: "B. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Up to 16Mbps.",
+choicec: "C. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choiced: "D. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+answer: "B"
+};
+
+examQuest[23] = {
+question: "CAT5 dat rate?",
+choicea: "A. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Up to 100 MHz/100Mbps, and 100 meters in length (295 feet)",
+choiceb: "B. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choicec: "C. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choiced: "D. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+answer: "A"
+};
+
+examQuest[24] = {
+question: "CAT5e data rate",
+choicea: "A. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choiceb: "B. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choicec: "C. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;100MHz/1000Mbps applications with improved noise performance in a full duplex mode",
+choiced: "D. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+answer: "C"
+};
+
+examQuest[25] = {
+question: "CAT6 data rate",
+choicea: "A. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choiceb: "B. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choicec: "C. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choiced: "D. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Up to 250MHz networks Class E/1000Mbps. Supports 10Gbps but at distances fewer than 100 meters.",
+answer: "D"
+};
+
+examQuest[26] = {
+question: "data rate for CAT6a",
+choicea: "A. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;up to 500MHz networks Class. Ea/10Gbps",
+choiceb: "B. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choicec: "C. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choiced: "D. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+answer: "A"
+};
+
+examQuest[27] = {
+question: "CAT7",
+choicea: "A. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choiceb: "B. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choicec: "C. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Up to 600MHz speed computer. Networks class F/10Gbps",
+choiced: "D. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+answer: "C"
+};
+
+examQuest[28] = {
+question: "CAT7a",
+choicea: "A. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choiceb: "B. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;up to 1000MHz speed computer. Networks class FA/10Gbps",
+choicec: "C. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choiced: "D. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+answer: "B"
+};
+
+examQuest[29] = {
+question: "Bit rate for OC-1 signal?",
+choicea: "A. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;51.840 Mbps",
+choiceb: "B. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choicec: "C. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choiced: "D. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+answer: "A"
+};
+
+examQuest[30] = {
+question: "Bit rate for OC-3 signal?",
+choicea: "A. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choiceb: "B. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choicec: "C. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choiced: "D. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;155.52",
+answer: "D"
+};
+
+examQuest[31] = {
+question: "Bit rate for OC-12?",
+choicea: "A. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;622.080 Mbps",
+choiceb: "B. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choicec: "C. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choiced: "D. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+answer: "A"
+};
+
+examQuest[32] = {
+question: "Bit rate for OC-48?",
+choicea: "A. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choiceb: "B. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;2.48832 Gbps",
+choicec: "C. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choiced: "D. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+answer: "B"
+};
+
+examQuest[33] = {
+question: "Bit rate for OC-192?",
+choicea: "A. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choiceb: "B. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choicec: "C. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;9.95328 Gbps",
+choiced: "D. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+answer: "C"
+};
