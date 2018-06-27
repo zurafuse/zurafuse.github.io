7311e47053d407b78ab16405c3fab4c85c04fc19
Timothy Horton <zurafuse@gmail.com>
2018-06-27 12:56:23 -0500

    Add files via upload

---
+++ networkexam5/questions.js
@@ -0,0 +1,412 @@
+var examQuest = [];
+
+examQuest[0] = {
+question: "Three steps in preparation for exam:",
+choicea: "A. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Content from Gary",
+choiceb: "B. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Security section of Ucertify",
+choicec: "C. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Quizes",
+choiced: "D. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Continue forward and don't worry.",
+answer: "D"
+};
+
+examQuest[1] = {
+question: "What is called a multiport repeater?",
+choicea: "A. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Gateway",
+choiceb: "B. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Hub",
+choicec: "C. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Switch",
+choiced: "D. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Router",
+answer: "B"
+};
+
+examQuest[2] = {
+question: "What is a table of MAC addresses and port mapping used by the switch to identify connected networking devices",
+choicea: "A. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;CAM (Content Addressable Memory)",
+choiceb: "B. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;ARP (Address Resolution Protocol)",
+choicec: "C. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;ACL (Access Control Lists)",
+choiced: "D. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;XML",
+answer: "A"
+};
+
+examQuest[3] = {
+question: "What is used to map an IP address to its MAC address?",
+choicea: "A. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;CAM (Content Addressable Memory)",
+choiceb: "B. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;ARP (Address Resolution Protocol)",
+choicec: "C. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;ACL (Access Control Lists)",
+choiced: "D. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;XML",
+answer: "B"
+};
+
+examQuest[4] = {
+question: "What is defined in the Data Link Layer, which is why the MAC address is sometimes called the Ethernet address?",
+choicea: "A. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;NIC",
+choiceb: "B. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Ethernet 802.11 standard",
+choicec: "C. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;WYSIWYG",
+choiced: "D. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Ethernet 802.3 standard",
+answer: "D"
+};
+
+examQuest[5] = {
+question: "What connects wireless devices together and is the connection point between wireless devices and a wired LAN?",
+choicea: "A. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Extended Service Set",
+choiceb: "B. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Router",
+choicec: "C. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;hot spot",
+choiced: "D. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Access Points",
+answer: "D"
+};
+
+examQuest[6] = {
+question: "What OSI layer accepts outgoing messages and combines messages or segments into packets, adding a header that includes routing information?",
+choicea: "A. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Physical Layer",
+choiceb: "B. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Network Layer",
+choicec: "C. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Data Link Layer",
+choiced: "D. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Transport Layer",
+answer: "B"
+};
+
+examQuest[7] = {
+question: "What is defined by the switch inputs marked with an 'x' and means the transmit and receive pairs on the twisted-pair cable are crossed to properly align each for data communication?",
+choicea: "A. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Straight-Through",
+choiceb: "B. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Multi-port repeater",
+choicec: "C. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Serial Port",
+choiced: "D. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Crossover",
+answer: "D"
+};
+
+examQuest[8] = {
+question: "Which 802 standards use MIMO?",
+choicea: "A. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;802.11a and 802.11n",
+choiceb: "B. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;802.11n and 802.11ac",
+choicec: "C. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;802.11b and 802.11g",
+choiced: "D. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;802.11a and 802.11b",
+answer: "B"
+};
+
+examQuest[9] = {
+question: "What is the physical connection where the router connects to the network (also called router ports)?",
+choicea: "A. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;network interface",
+choiceb: "B. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;data link layer",
+choicec: "C. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;router interface",
+choiced: "D. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;network interface card",
+answer: "C"
+};
+
+examQuest[10] = {
+question: "Direct-Sequence Spread Spectrum (DSSS) is supported by what?",
+choicea: "A. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;802.11b and 802.11g",
+choiceb: "B. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;802.11n and 802.11ac",
+choicec: "C. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;802.11b and 802.11n",
+choiced: "D. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;802.11a and 802.11b",
+answer: "A"
+};
+
+examQuest[11] = {
+question: "What is a SYN Flood?",
+choicea: "A. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;a form of denial-of-service attack in which an attacker sends a succession of ACK requests to a target's system in an attempt to take control of the target system.",
+choiceb: "B. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;a form of denial-of-service attack in which an attacker sends a succession of ping requests to a target's system in an attempt to consume enough server resources to make the system crash.",
+choicec: "C. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;a form of denial-of-service attack in which an attacker sends a succession of SYN requests to a target's system in an attempt to consume enough server resources to make the system unresponsive to legitimate traffic.",
+choiced: "D. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Too much water",
+answer: "C"
+};
+
+examQuest[12] = {
+question: "Edge technology speed",
+choicea: "A. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;256Kbps",
+choiceb: "B. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;512Kbps",
+choicec: "C. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;mock 3",
+choiced: "D. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;384Kbps",
+answer: "D"
+};
+
+examQuest[13] = {
+question: "What the air interface protocol adopted for RFID tags, a network communications protocol technique similar to the Ethernet protocol? the tags are only allowed to transmit at predetermined times after being energized. This technique reduces the chance of data collisions between RFID tag transmissions and allows for the reading of up to 1000 tags per second.",
+choicea: "A. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;IKE",
+choiceb: "B. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;backscatter",
+choicec: "C. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;RADIUS",
+choiced: "D. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Slotted Aloha",
+answer: "D"
+};
+
+examQuest[14] = {
+question: "Packet Acknowledgement - in an SYN ACK packet, Host B replies with a SYN ACK packet. The ACK is an acknowledgement that Host B received the packet from Host A. A number is attached to the ACK with a value of (x + 1) that should be the sum of the SEQ# from packet 1 plus the length (LEN) of packet 1. What does this mean? This acknowledgement notifies Host A that the packet (packet 1) was received. Explaining how a host knows whether a data packet was not received, The host looks at the ACK number and SEQ number in the packet. If a SYN ACK packet has an ACK number that is the SEQ number (of the previous packet) plus 1, what does that indicate?",
+choicea: "A. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;the previous packet was not received.",
+choiceb: "B. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;the previous packet was received.",
+choicec: "C. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;the packet is intended for a different destination.",
+choiced: "D. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;time travel",
+answer: "B"
+};
+
+examQuest[15] = {
+question: "How does CSMA/CD detect collisions?",
+choicea: "A. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;It doesn't. It avoids them.",
+choiceb: "B. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;By having fast reflexes and moving quickly.",
+choicec: "C. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;by detecting a collision in the medium and backing off (after transmitting a jam signal) as necessary. Assume, we have a LAN with a number of hosts. When one host wants to transmit a frame, it listens on the medium to check if the medium is busy. Note that absence of any current (or the presence of a residual background current) means no one else is transmitting.",
+choiced: "D. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Infrared technology.",
+answer: "C"
+};
+
+examQuest[16] = {
+question: "What supports BOTH OFDM and DSSS?",
+choicea: "A. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;802.11b",
+choiceb: "B. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;802.11n",
+choicec: "C. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;802.11ac",
+choiced: "D. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;802.11g",
+answer: "D"
+};
+
+examQuest[17] = {
+question: "What is called a muItiport bridge?",
+choicea: "A. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;gateway",
+choiceb: "B. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;router",
+choicec: "C. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;switch",
+choiced: "D. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;hub",
+answer: "C"
+};
+
+examQuest[18] = {
+question: "What gives out private IP addresses and translates the private IP address to a public address for routing over the Internet?",
+choicea: "A. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Network Address Translation (NAT)",
+choiceb: "B. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Address Resolution Protocol (ARP)",
+choicec: "C. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Content Addressable Memory (CAM)",
+choiced: "D. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;the dictionary",
+answer: "A"
+};
+
+examQuest[19] = {
+question: "Orthogonal Frequency Division Multiplexing (OFDM) is supported by what?",
+choicea: "A. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;802.11b, 802.11g, 802.11ac",
+choiceb: "B. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;802.11a, 802.11ac, 802.11n",
+choicec: "C. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;802.11b, 802.11g, 802.11n",
+choiced: "D. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;802.11a, 802.11g, and 802.11n",
+answer: "D"
+};
+
+examQuest[20] = {
+question: "What is 10Base-T?",
+choicea: "A. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;100Mbps over Twisted Pair",
+choiceb: "B. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;1000Mbps over Twisted Pair",
+choicec: "C. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;10Mbps over Twisted Pair",
+choiced: "D. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;1Gbps over Twisted Pair",
+answer: "C"
+};
+
+examQuest[21] = {
+question: "The subnet mask for a CIDR of /22 is which of the following?",
+choicea: "A. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;255.255.254.0",
+choiceb: "B. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;255.255.255.0",
+choicec: "C. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;255.255.192.0",
+choiced: "D. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;255.255.252.0",
+answer: "D"
+};
+
+examQuest[22] = {
+question: "Subnet mask for CIDR /21",
+choicea: "A. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;255.255.252.0",
+choiceb: "B. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;255.255.248.0",
+choicec: "C. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;255.255.192.0",
+choiced: "D. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;255.255.254.0",
+answer: "B"
+};
+
+examQuest[23] = {
+question: "A network address of 192.168.6.0 and a subnet mask of 255.255.254.0 can be written in CIDR as which of the following?",
+choicea: "A. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;192.168.6.0/23",
+choiceb: "B. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;192.168.6.0/24",
+choicec: "C. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;192.168.6.0/22",
+choiced: "D. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;192.255.6.0/23",
+answer: "A"
+};
+
+examQuest[24] = {
+question: "When Data packets are sent to the entire group of networking devices, such as a group of routers running the same routing protocol",
+choicea: "A. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Unicast Address",
+choiceb: "B. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Multicast Address",
+choicec: "C. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Singlecast Address",
+choiced: "D. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;unicorns",
+answer: "B"
+};
+
+examQuest[25] = {
+question: "How can you match a MAC address with the IP address?",
+choicea: "A. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Use Content Addressable Memory (CAM)",
+choiceb: "B. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;if the computers are connected to the same TCP/IP network, you can determine the MAC address through a technology called ARP (Address Resolution Protocol), which is included with TCP/IP. Using ARP, each local network interface tracks both the IP address and MAC address for each device it has recently communicated with. Most computers let you see this list of addresses that ARP has collected.",
+choicec: "C. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Look for it really hard.",
+choiced: "D. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Look it up in a network table on the switch.",
+answer: "B"
+};
+
+examQuest[26] = {
+question: "Wi Max range",
+choicea: "A. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;1GHz-50GHz",
+choiceb: "B. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;30GHz",
+choicec: "C. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;100GHz",
+choiced: "D. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;2GHz-66GHz",
+answer: "D"
+};
+/*
+examQuest[27] = {
+question: "text",
+choicea: "A. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choiceb: "B. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choicec: "C. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choiced: "D. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+answer: "X"
+};
+
+examQuest[#] = {
+question: "text",
+choicea: "A. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choiceb: "B. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choicec: "C. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choiced: "D. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+answer: "X"
+};
+
+
+
+
+ARIN
+American Registry for Internet Numbers
+
+
+SONET
+Synchronous optical network
+
+
+PATCH CABLE
+A patch cable (item G) is used to make the physical connection from the computer to the wall plate. A patch cable is a short cable used to make the physical connection between networking equipment. There is a 100-meter overall length limitation of the cable run from the telecommunications closet to the networking device in the work area. This includes the length of the patch cables at each end plus the cable run. A general rule of thumb is to allow 90 meters for the cable run from the telecommunications closet to the work area (item E). This allows 5 meters of cable length for the work area and 5 meters for the patch cables in the telecommunications closet (item D) and the work area (item G). Figure 2-5 shows an example of the insides of a telecommunications closet.
+
+
+ports
+DNS port is 53
+Telnet port is 23
+L2TP port is 1701
+POP3 port is 110
+
+Fraggle
+Denial of Service (DOS) attack in which spoofed UPD packets are sent to a network's broadcast address.
+
+
+On a network what is the broadcast address?
+logical address at which all devices connected to a multiple-access communications network are enabled to receive datagrams
+
+
+What is a network address?
+any logical or physical address that uniquely distinguishes a network node or device over a computer or telecommunications network
+
+
+Layout of Ethernet frame?
+Preamble, Start Frame Delimiter, Destination MAC address, Source MAC address, Length Type, Data, Pad, Frame check sequence
+
+
+What does the Frame check sequence do in an Ethernet frame?
+A 4-byte CRC value used for error detection. The CRC is performed on the bits from the destination MAC address through the Pad fields. If an error is detected, the frame is discarded.
+
+
+If you want to subnet a class C network into four subnets, the subnet mask used should be what?
+255.255.255.192
+
+
+the Equipment Room
+Cabling that interconnects telecommunication closets in the same building and between buildings
+
+
+NEXT
+A measure of the level of crosstalk or signal coupling within the cable, with a high NEXT (dB) value being desirable.
+
+
+Backbone cabling
+the cabling that interconnects telecommunication closets, equipment rooms, and cabling entrances in the same building and between buildings
+
+
+
+
+
+ACL
+An Access List. A basic form of firewall protection. Assists in Packet Filtering.
+
+
+
+IPSEC
+encrypts for VPN tunnels.
+
+
+Dispersion Types
+chromatic dispersion: The broadening of a pulse due to different propagation velocities of the spectral components of the light pulse.
+
+polarized dispersion: The broadening of a pulse due to the different propagation velocities of the X and Y polarization components of the light pulse.
+
+modal dispersion: The broadening of a pulse due to different path lengths taken through the fiber by different modes.
+
+
+Which layer uses hardware to handle switching functions?
+Layer 3 switches
+
+
+TKIP
+a wireless protocol not used by IPSEC
+
+
+Smurf attack
+a distributed denial-of-service attack in which large numbers of Internet Control Message Protocol (ICMP) packets with the intended victim's spoofed source IP are broadcast to a computer network using an IP broadcast address.
+
+spoofing attack
+when a malicious party impersonates another device or user on a network in order to launch attacks against network hosts, steal data, spread malware or bypass access controls.
+
+
+Two types of shielded twisted pair used today:
+straight-through: wires connect to same pins
+
+cross-connect: wires connect to different pins. Transmit and receiver wire pairs are crossed.
+
+
+PPP
+the de facto standard in dial-up VPNs.
+
+Store-and-Forward:
+In this mode, the entire frame of data is received before any decision is made regarding forwarding the data packet to its destination.
+
+cut-through:
+In this mode, the data packet is forwarded to the destination as soon as the destination MAC address has been read. This minimizes the switch latency; however, no error detection is provided by the switch. There are two forms of cut-through switching—Fast-Forward and Fragment Free.
+
+
+The Network File System (NFS)
+a client/server application that lets a computer user view and optionally store and update files on a remote computer as though they were on the user's own computer. The NFS protocol is one of several distributed file system standards for network-attached storage (NAS). This runs on the session layer of the OSI model.
+
+
+Fiber wavelengths
+Multimode wavelength: 850 and 1300 nm
+
+Singlemode wavelength: 1310 and 1550 nm
+
+Fiber to the home: 1600-1625 nm
+
+
+Auto Negotiation
+Automatically setups network stuff. Relies on fast link pulses to do the job. Chooses the lower data rate when connecting things.
+
+
+Bridge
+a computer networking device that creates a single aggregate network from multiple communication networks or network segments. This function is called network bridging. ... In the OSI model, bridging is performed in the data link layer (layer 2), below the network layer (layer 3).
+
+Router
+allows connection to hosts and networks outside the LAN
+
+Switch
+Forwards a frame it receives directly out the port associated with its destination address
+
+Hub
+Broadcasts the data it receives to all devices connected to its ports
+
+Gateway
+Describes the networking device that enables hosts in a LAN to connect to networks (and hosts) outside the LAN
+
+
+
+
+antennas
+An omni (omni-directional antenna) has a 360 degree pattern and supports a 2Mbps data rate up to 7 km from the antenna and supports an 11Mbps data rate at a maximum distance of 2 km.
+
+The Yagi antenna has a directional radiation pattern. It supports a 2Mbps data rate for a maximum of 12 km, and supports an 11Mbps data rate up to 7.5 km.
+
+A dish antenna is a parabolic reflector. These antennas provide extremely high directional gain. These have a 38 km range at 2Mbps, an 18 km range at 11Mbps, and a high cost.
+*/
