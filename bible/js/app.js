7cccac3fb1caa7a54cae84eefb150cd35a1e7cde
Timothy Horton <zurafuse@gmail.com>
2018-07-06 22:34:53 -0500

    Add files via upload

--- bible/js/app.js
+++ bible/js/app.js
@@ -1,1 +1,28 @@
 
+//Populate Bible menu with books.
+for (i = 0; i < Torah.BOOKS.length; i++)
+{
+	$(".bible-menu").append("<option>" + Torah.BOOKS[i].bname + "</option>");
+}
+
+for (i = 0; i < Prophets.BOOKS.length; i++)
+{
+	$(".bible-menu").append("<option>" + Prophets.BOOKS[i].bname + "</option>");
+}
+
+$(".bible-menu").append("<option>Psalms</option>");
+
+for (i = 0; i < Ketuvim.BOOKS.length; i++)
+{
+	$(".bible-menu").append("<option>" + Ketuvim.BOOKS[i].bname + "</option>");
+}
+
+for (i = 0; i < Gospel.BOOKS.length; i++)
+{
+	$(".bible-menu").append("<option>" + Gospel.BOOKS[i].bname + "</option>");
+}
+
+for (i = 0; i < NewTestament.BOOKS.length; i++)
+{
+	$(".bible-menu").append("<option>" + NewTestament.BOOKS[i].bname + "</option>");
+}
