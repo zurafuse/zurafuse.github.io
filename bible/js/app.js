2f9f3fd79838d8e9039985818ab6091612e7bd67
Timothy Horton <zurafuse@gmail.com>
2018-07-10 13:34:49 -0500

    Update app.js

--- bible/js/app.js
+++ bible/js/app.js
@@ -54,19 +54,19 @@ function displayVerses(chapter){
 }
 
 $( ".chapters" ).on("click", function() {
-  displayVerses(book.CHAPTER[parseInt($(this).text().replace("CHAPTER ", ""))]);
+  displayVerses(Genesis.CHAPTER[parseInt($(this).text().replace("CHAPTER ", ""))]);
 });
 
 setTimeout(function(){
 	$(".chapters").off("click");
 	$( ".chapters" ).on("click", function() {
-	displayVerses(book.CHAPTER[parseInt($(this).text().replace("CHAPTER ", ""))]);
+	displayVerses(Genesis.CHAPTER[parseInt($(this).text().replace("CHAPTER ", ""))]);
 	});
 }, 500);
 
 setTimeout(function(){
 	$(".chapters").off("click");
 	$( ".chapters" ).on("click", function() {
-  	displayVerses(book.CHAPTER[parseInt($(this).text().replace("CHAPTER ", ""))]);
+  	displayVerses(Genesis.CHAPTER[parseInt($(this).text().replace("CHAPTER ", ""))]);
 	});
 }, 1300);
