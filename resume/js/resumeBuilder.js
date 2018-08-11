778f2a369dcaac86575d36117dd87c18b446c8fa
Timothy Horton <zurafuse@gmail.com>
2018-08-11 10:50:05 -0500

    Update resumeBuilder.js

--- resume/js/resumeBuilder.js
+++ resume/js/resumeBuilder.js
@@ -127,7 +127,7 @@ var work =
 			title: "Software Engineer/Programmer Analyst", 
 			location: "Montgomery, AL", 
 			dates: "September 2016 - present",
-			description: "Duties include developing forms for the company, performing research and business analysis needed to do so, monitoring the SmartCommunications (Thunderhead) system and database, and writing javascript code to integrate SmartCommunications with other systems. As needed, I write web apps and .NET desktop applications to help the computer operators and the other forms developers perform specific tasks." 		
+			description: "Duties include developing forms for the company, performing research and business analysis needed to do so, monitoring the SmartCommunications (Thunderhead) system and database, and writing javascript code to integrate SmartCommunications with other systems. As needed, I write web apps and .NET desktop applications to help the computer operators and the other forms developers perform specific tasks. In 2018, I assisted with the company's implementation of an e-signature solution, DocuSign." 		
 		},
 		{
 			employer: "ALFA Insurance Companies", 
