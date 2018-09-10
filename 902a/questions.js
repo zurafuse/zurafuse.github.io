0eff735244673603e8ad12e81d4181ad0368ffc7
Timothy Horton <zurafuse@gmail.com>
2018-09-10 11:18:53 -0500

    Add files via upload

---
+++ 902a/questions.js
@@ -0,0 +1,487 @@
+var examQuest = [];
+
+examQuest[0] = {
+question: "To create a 500 GB partition, how much space should you allocate?",
+choicea: "A. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;500000 MB",
+choiceb: "B. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;512000 GB",
+choicec: "C. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;512000 MB",
+choiced: "D. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;500000 GB",
+answer: "C"
+ };
+ 
+examQuest[1] = {
+question: "What does 'RD [drive:][path]' do?",
+choicea: "A. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choiceb: "B. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choicec: "C. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choiced: "D. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Removes (deletes) the specified directory.",
+answer: "D"
+ };
+ 
+examQuest[2] = {
+question: "What does 'CD [path]' do?",
+choicea: "A. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choiceb: "B. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choicec: "C. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Changes to the specified directory",
+choiced: "D. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+answer: "C"
+ };
+ 
+examQuest[3] = {
+question: "Which of the following key combinations invokes the Task Manager in a Windows 8 computer?",
+choicea: "A. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;CTRL + SHIFT + ESC",
+choiceb: "B. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choicec: "C. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choiced: "D. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+answer: "A"
+ };
+ 
+examQuest[4] = {
+question: "What is a technology that configures computers to optimize power consumption?",
+choicea: "A. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choiceb: "B. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choicec: "C. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choiced: "D. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Advanced Configuration and Power Interface (ACPI)",
+answer: "D"
+ };
+ 
+examQuest[5] = {
+question: "Which of the following switches can be used with ROBOCOPY to mirror a complete directory tree?",
+choicea: "A. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choiceb: "B. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;/MIR",
+choicec: "C. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choiced: "D. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+answer: "B"
+ };
+ 
+examQuest[6] = {
+question: "Which of the following switches will you use with the xcopy command to display the names of all the files that would be copied?",
+choicea: "A. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choiceb: "B. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choicec: "C. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;/f",
+choiced: "D. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+answer: "C"
+ };
+ 
+ examQuest[7] = {
+question: "To create a 250GB partition, how much space should you allocate?",
+choicea: "A. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;250000 MB",
+choiceb: "B. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;256000 MB",
+choicec: "C. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;256000 GB",
+choiced: "D. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;512000 MB",
+answer: "B"
+ };
+ 
+examQuest[8] = {
+question: "What is the indication of a command line switch?",
+choicea: "A. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;back slash \\",
+choiceb: "B. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;forward slash /",
+choicec: "C. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;?",
+choiced: "D. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;your face",
+answer: "B"
+ };
+ 
+examQuest[9] = {
+question: "What utility can be used to schedule a shutdown (complete or a restart), locally or remotely?",
+choicea: "A. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;SHUTDOWN.EXE",
+choiceb: "B. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;/s /r",
+choicec: "C. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;TASKSCHEDULER.EXE",
+choiced: "D. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;MSCONFIG.EXE",
+answer: "A"
+ };
+ 
+ examQuest[10] = {
+question: "Create by Microsoft as a proprietary filesystem of choice for flash drives where typical format cannot be used (because of overhead) and legacy is not acceptable (due to filesystem limitations). It is ideal for SD cards that hold a lot of information, and it is supported in all current versions of Windows.",
+choicea: "A. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Network File System (NFS)",
+choiceb: "B. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;FAT (File Allocation Table)",
+choicec: "C. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;ExFAT (Extended File Allocation Table)",
+choiced: "D. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;FAT32",
+answer: "C"
+ };
+ 
+examQuest[11] = {
+question: "open computer management",
+choicea: "A. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;compmgmt.msc",
+choiceb: "B. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choicec: "C. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;msconfig",
+choiced: "D. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+answer: "A"
+ };
+ 
+examQuest[12] = {
+question: "Changes to the directory that is up one level.",
+choicea: "A. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choiceb: "B. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;CD . .",
+choicec: "C. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choiced: "D. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;CD\\",
+answer: "B"
+ };
+ 
+examQuest[13] = {
+question: "Changes to the root directory of the drive.",
+choicea: "A. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choiceb: "B. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choicec: "C. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choiced: "D. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;CD\\",
+answer: "D"
+ };
+ 
+examQuest[14] = {
+question: "Which of the following can be used to configure a remote connection?",
+choicea: "A. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;MSTSC",
+choiceb: "B. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choicec: "C. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choiced: "D. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+answer: "A"
+ };
+ 
+examQuest[15] = {
+question: "The ___ tool is used to decide whether defragmentation of a drive is recommended or not.",
+choicea: "A. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choiceb: "B. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choicec: "C. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;defrag",
+choiced: "D. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+answer: "C"
+ };
+ 
+examQuest[16] = {
+question: "created by Sun Microsystems a number of decades ago, and it is widely used in Unix and then Linux environments. Starting with Windows 7, Microsoft includes support for it with Windows (but it is not always installed). Windows versions prior to Windows 7 (Vista) can install Windows Services for Unix to gain this support.",
+choicea: "A. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choiceb: "B. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choicec: "C. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Network File System (NFS)",
+choiced: "D. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+answer: "C"
+ };
+ 
+examQuest[17] = {
+question: "What key combination will allow you to run as administrator?",
+choicea: "A. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choiceb: "B. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choicec: "C. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;CTRL + SHIFT + ENTER",
+choiced: "D. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+answer: "C"
+ };
+ 
+examQuest[18] = {
+question: "The utility used to terminate processes. The process can exist on the machine where the administrator is sitting (the default) or on another machine, in which case you signify the other system by using the /S switch.",
+choicea: "A. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choiceb: "B. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choicec: "C. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choiced: "D. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;TASKKILL.EXE",
+answer: "D"
+ };
+ 
+ examQuest[19] = {
+question: "How do you inquire to learn about a command or its switches?",
+choicea: "A. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choiceb: "B. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choicec: "C. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;/?",
+choiced: "D. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+answer: "C"
+ };
+ 
+ examQuest[20] = {
+question: "COPY switch that indicates an ASCII text file.",
+choicea: "A. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choiceb: "B. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choicec: "C. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;/a",
+choiced: "D. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+answer: "C"
+ };
+ 
+ examQuest[21] = {
+question: "Processes terminated by TASKKILL can be identified by either name or ___",
+choicea: "A. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;process ID number (PID)",
+choiceb: "B. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choicec: "C. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choiced: "D. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+answer: "A"
+ };
+ 
+ examQuest[22] = {
+question: "Not natively supported with Windows, it became the default filesystem for many distributions of Linux due to its journaling capabilities.",
+choicea: "A. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choiceb: "B. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choicec: "C. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choiced: "D. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Extended File System (ext)",
+answer: "D"
+ };
+ 
+ examQuest[23] = {
+question: "COPY switch that Verifies that new files are written correctly.",
+choicea: "A. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choiceb: "B. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;/v",
+choicec: "C. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choiced: "D. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+answer: "B"
+ };
+ 
+ examQuest[24] = {
+question: "COPY switch that allows you to avoid the YES/NO prompt.",
+choicea: "A. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;/y",
+choiceb: "B. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choicec: "C. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choiced: "D. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+answer: "A"
+ };
+ 
+ examQuest[25] = {
+question: "Command used to format a partition. Available with all versions of Windows. You can run it from a command prompt or by right-clicking a drive in Windows File Explorer and selecting it as an option.",
+choicea: "A. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;FORMAT.EXE",
+choiceb: "B. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choicec: "C. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choiced: "D. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+answer: "A"
+ };
+ 
+ examQuest[26] = {
+question: "What in the command prompt will show you a list of folders?",
+choicea: "A. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;DIR",
+choiceb: "B. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choicec: "C. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choiced: "D. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+answer: "A"
+ };
+ 
+ examQuest[27] = {
+question: "Removes all directories and files in the specified directory, including the specified directory itself. Used to remove an entire tree.",
+choicea: "A. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choiceb: "B. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;RD /S [drive:][path]",
+choicec: "C. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choiced: "D. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+answer: "B"
+ };
+ 
+ examQuest[28] = {
+question: "Quiet mode. You won't be asked whether you're sure you want to delete the specified directory when you use /S.",
+choicea: "A. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choiceb: "B. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choicec: "C. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;RD /Q [drive:][path]",
+choiced: "D. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+answer: "C"
+ };
+ 
+ examQuest[29] = {
+question: "Command to erase or delete files",
+choicea: "A. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;DEL",
+choiceb: "B. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choicec: "C. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choiced: "D. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+answer: "A"
+ };
+ 
+ examQuest[30] = {
+question: "DEL switch that Prompts for confirmation before deleting each file.",
+choicea: "A. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choiceb: "B. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;/p",
+choicec: "C. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choiced: "D. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+answer: "B"
+ };
+ 
+ examQuest[31] = {
+question: "SHUTDOWN switch to set the timeout period (in seconds).",
+choicea: "A. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choiceb: "B. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choicec: "C. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;/t xxx",
+choiced: "D. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+answer: "C"
+ };
+ 
+ examQuest[32] = {
+question: "DEL switch to Force deleting of read-only files.",
+choicea: "A. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;/f",
+choiceb: "B. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choicec: "C. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choiced: "D. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+answer: "A"
+ };
+ 
+ examQuest[33] = {
+question: "SHUTDOWN switch to power down with no timeout or warning.",
+choicea: "A. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choiceb: "B. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choicec: "C. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;/p",
+choiced: "D. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+answer: "C"
+ };
+ 
+ examQuest[34] = {
+question: "DEL switch that Selects files to delete based on attributes",
+choicea: "A. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choiceb: "B. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choicec: "C. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;/a",
+choiced: "D. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+answer: "C"
+ };
+ 
+ examQuest[35] = {
+question: "While not a filesystem that can be used on a hard drive, this is the filesystem of choice for CD media, and it has been used with 32-bit Windows versions since Windows 95. It appears as a collection.",
+choicea: "A. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choiceb: "B. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choicec: "C. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choiced: "D. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;CD-ROM File System (CDFS)",
+answer: "D"
+ };
+ 
+ examQuest[36] = {
+question: "Changes to the specified directory on the drive.",
+choicea: "A. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choiceb: "B. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;CD /D [drive:][path]",
+choicec: "C. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choiced: "D. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+answer: "B"
+ };
+ 
+ examQuest[37] = {
+question: "TASKLIST switch that Specifies the remote system to connect to.",
+choicea: "A. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choiceb: "B. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choicec: "C. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;/s",
+choiced: "D. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+answer: "C"
+ };
+ 
+ examQuest[38] = {
+question: "Introduced along with Windows NT, this is available with all current versions of Windows. It is a much more advanced filesystem in almost every way than all versions of the FAT filesystem. It includes such features as individual file security and compression and RAID support as well as support for extremely large file and partition sizes and disk transaction monitoring. It is the filesystem of choice for higher-performance computing.",
+choicea: "A. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choiceb: "B. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choicec: "C. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;NTFS",
+choiced: "D. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+answer: "C"
+ };
+ 
+ examQuest[39] = {
+question: "The utility that interacts with the Master Boot Record (MBR), boot sector, or Boot Configuration Data (BCD) store. To run the tool, you must boot from the installation disk, choose the Repair Your Computer option, and enter the System Recovery Options. Choose Command Prompt from System Recovery Options and then type the command.",
+choicea: "A. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choiceb: "B. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;BOOTREC",
+choicec: "C. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choiced: "D. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+answer: "B"
+ };
+ 
+ examQuest[40] = {
+question: "Command to check digital signatures",
+choicea: "A. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choiceb: "B. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choicec: "C. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;SFC",
+choiced: "D. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+answer: "C"
+ };
+ 
+ examQuest[41] = {
+question: "TASKLIST switch that Specifies the user context under which the command should execute.",
+choicea: "A. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;/u",
+choiceb: "B. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choicec: "C. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choiced: "D. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+answer: "A"
+ };
+ 
+ examQuest[42] = {
+question: "BOOTREC option to rebuild the BCD store",
+choicea: "A. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choiceb: "B. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choicec: "C. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choiced: "D. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;/RebuildBCD",
+answer: "D"
+ };
+ 
+ examQuest[43] = {
+question: "What does NTFS stand for?",
+choicea: "A. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;New Technology File System",
+choiceb: "B. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choicec: "C. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choiced: "D. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+answer: "A"
+ };
+ 
+ examQuest[44] = {
+question: "BOOTREC option to scan all disks for installations the Boot Manager menu is not listing",
+choicea: "A. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;/ScanOS",
+choiceb: "B. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choicec: "C. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choiced: "D. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+answer: "S"
+ };
+ 
+ examQuest[45] = {
+question: "TASKLIST switch that Displays verbose task information.",
+choicea: "A. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;/v",
+choiceb: "B. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choicec: "C. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choiced: "D. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+answer: "A"
+ };
+ 
+ examQuest[46] = {
+question: "Which of the following Windows tools is used to resolve file system issues?",
+choicea: "A. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choiceb: "B. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;chkdsk",
+choicec: "C. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choiced: "D. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+answer: "B"
+ };
+ 
+ examQuest[47] = {
+question: "What bootrec.exe option writes a new boot sector?",
+choicea: "A. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choiceb: "B. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;/Fixboot",
+choicec: "C. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choiced: "D. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+answer: "B"
+ };
+ 
+ examQuest[48] = {
+question: "What bootrec.exe option writes a new MBR",
+choicea: "A. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choiceb: "B. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choicec: "C. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choiced: "D. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;/Fixmbr",
+answer: "D"
+ };
+ 
+ examQuest[49] = {
+question: "TASKLIST switch that prompts for a password.",
+choicea: "A. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choiceb: "B. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choicec: "C. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choiced: "D. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;/p",
+answer: "D"
+ };
+ 
+ examQuest[50] = {
+question: "Disks that use Unified Extensible Firmware Interface (UEFI). They will replace master boot record in the new world.",
+choicea: "A. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choiceb: "B. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choicec: "C. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choiced: "D. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;GUID Partition Table (GPT)",
+answer: "D"
+ };
+ 
+ examQuest[51] = {
+question: "System that replaces the BIOS in many modern systems, making BIOS legacy.",
+choicea: "A. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choiceb: "B. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choicec: "C. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;UEFI",
+choiced: "D. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+answer: "C"
+ };
+ 
+ examQuest[52] = {
+question: "TASKLIST switch that Lists all tasks currently using the given exe/dll name.",
+choicea: "A. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choiceb: "B. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;/m",
+choicec: "C. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choiced: "D. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+answer: "B"
+ };
+
+ examQuest[53] = {
+question: "acronym for the file table used to keep track of where files are within the filesystem.",
+choicea: "A. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;File allocation table (FAT)",
+choiceb: "B. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choicec: "C. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choiced: "D. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+answer: "A"
+ }; 
