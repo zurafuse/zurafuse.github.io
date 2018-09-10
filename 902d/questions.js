feae0f38e2c838695cdba82952412864a8399f3d
Timothy Horton <zurafuse@gmail.com>
2018-09-10 11:25:38 -0500

    Add files via upload

---
+++ 902d/questions.js
@@ -0,0 +1,245 @@
+var examQuest = [];
+
+examQuest[0] = {
+question: "Linux command to view or configure an interface and IP configuration.",
+choicea: "A. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choiceb: "B. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choicec: "C. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;ifconfig",
+choiced: "D. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+answer: "C"
+ };
+
+ examQuest[1] = {
+question: "Third extended file system - commonly used by Linux",
+choicea: "A. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choiceb: "B. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choicec: "C. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choiced: "D. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;ext3",
+answer: "D"
+ };
+ 
+ examQuest[2] = {
+question: "From which version of Windows XP can you upgrade to any Vista edition?",
+choicea: "A. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choiceb: "B. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choicec: "C. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Home",
+choiced: "D. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+answer: "C"
+ };
+ 
+ examQuest[3] = {
+question: "From Windows XP Professional, you can upgrade to which editions of Vista?",
+choicea: "A. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choiceb: "B. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Business and Ultimate",
+choicec: "C. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choiced: "D. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+answer: "B"
+ };
+ 
+ examQuest[4] = {
+question: "Linux command to view the current process",
+choicea: "A. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choiceb: "B. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choicec: "C. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choiced: "D. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;ps",
+answer: "D"
+ };
+ 
+ examQuest[5] = {
+question: "From Windows XP Media Center, you can upgrade to which editions of Vista?",
+choicea: "A. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choiceb: "B. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choicec: "C. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choiced: "D. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Home Premium and Ultimate",
+answer: "D"
+ };
+ 
+ examQuest[6] = {
+question: "From any edition of Windows XP, what editions of Windows 7 can you upgrade to?",
+choicea: "A. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choiceb: "B. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;none",
+choicec: "C. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choiced: "D. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+answer: "B"
+ };
+ 
+ examQuest[7] = {
+question: "Which editions of Windows 7 can Vista Home Basic upgrade to?",
+choicea: "A. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Home Basic, Home Premium, and Ultimate",
+choiceb: "B. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choicec: "C. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choiced: "D. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+answer: "A"
+ };
+ 
+ examQuest[8] = {
+question: "Which editions of Windows 7 can Vista Home Premium upgrade to?",
+choicea: "A. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choiceb: "B. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choicec: "C. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choiced: "D. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Home Premium and Ultimate",
+answer: "D"
+ };
+ 
+ examQuest[9] = {
+question: "Linux command to become super user",
+choicea: "A. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;su",
+choiceb: "B. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choicec: "C. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choiced: "D. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+answer: "A"
+ };
+ 
+ examQuest[10] = {
+question: "Which editions of Windows 7 can Vista Business upgrade to?",
+choicea: "A. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choiceb: "B. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Professional, Ultimate, and Enterprise",
+choicec: "C. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choiced: "D. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+answer: "B"
+ };
+ 
+ examQuest[11] = {
+question: "Which edition of Windows 7 can Vista Ultimate upgrade to?",
+choicea: "A. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choiceb: "B. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choicec: "C. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Ultimate",
+choiced: "D. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+answer: "C"
+ };
+ 
+ examQuest[12] = {
+question: "Linux command to execute a command as the super user",
+choicea: "A. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choiceb: "B. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;sudo",
+choicec: "C. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choiced: "D. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+answer: "B"
+ };
+ 
+ examQuest[13] = {
+question: "Which edition of Windows 7 can Vista Enterprise upgrade to?",
+choicea: "A. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choiceb: "B. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choicec: "C. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Enterprise",
+choiced: "D. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+answer: "C"
+ };
+
+ examQuest[14] = {
+question: "Which Windows 7 editions can upgrade to Windows 8 Core and Pro, but not Enterprise?",
+choicea: "A. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choiceb: "B. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Starter, Home Basic, and Home Premium",
+choicec: "C. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choiced: "D. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+answer: "B"
+ };
+ 
+ examQuest[15] = {
+question: "Which Edition of Windows 7 can upgrade to Windows 8 Pro and Enterprise?",
+choicea: "A. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Professional",
+choiceb: "B. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choicec: "C. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choiced: "D. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+answer: "A"
+ };
+ 
+ examQuest[16] = {
+question: "Linux command for Advanced Packaging Tools - handles management of application packages.",
+choicea: "A. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choiceb: "B. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choicec: "C. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;apt-get",
+choiced: "D. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+answer: "C"
+ };
+ 
+ examQuest[17] = {
+question: "Which version of Windows 7 can only upgrade to Windows 8 Pro?",
+choicea: "A. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choiceb: "B. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choicec: "C. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Ultimate",
+choiced: "D. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+answer: "C"
+ };
+ 
+ examQuest[18] = {
+question: "Which Windows 7 Edition can only upgrade to Windows 8 Enterprise?",
+choicea: "A. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choiceb: "B. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choicec: "C. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choiced: "D. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Enterprise",
+answer: "D"
+ };
+ 
+ examQuest[19] = {
+question: "What Windows 8.1 editions can you upgrade to from 8 Core?",
+choicea: "A. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choiceb: "B. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Core and Pro",
+choicec: "C. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choiced: "D. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+answer: "B"
+ };
+ 
+ examQuest[20] = {
+question: "What Windows 8.1 editions can you upgrade to from 8 Pro?",
+choicea: "A. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Pro and Enterprise",
+choiceb: "B. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choicec: "C. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choiced: "D. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+answer: "A"
+ };
+ 
+ examQuest[21] = {
+question: "What Windows 8.1 edition can you upgrade to from 8 Enterprise?",
+choicea: "A. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choiceb: "B. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choicec: "C. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choiced: "D. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Enterprise",
+answer: "D"
+ };
+ 
+ examQuest[22] = {
+question: "Pixie Preboot Execution Environment",
+choicea: "A. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choiceb: "B. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choicec: "C. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;PXE",
+choiced: "D. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+answer: "C"
+ };
+ 
+ examQuest[23] = {
+question: "Self-Monitoring and Analytical technology for disks.",
+choicea: "A. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choiceb: "B. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;S.M.A.R.T.",
+choicec: "C. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choiced: "D. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+answer: "B"
+ };
+ 
+ examQuest[24] = {
+question: "Integrates Apple devices and backs up iOS environments",
+choicea: "A. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;iCloud",
+choiceb: "B. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choicec: "C. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choiced: "D. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+answer: "A"
+ };
+ 
+ examQuest[25] = {
+question: "Linux command for Visual Mode Editor (full screen editing with copy, paste, etc.)",
+choicea: "A. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choiceb: "B. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choicec: "C. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choiced: "D. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;vi",
+answer: "D"
+ };
+ 
+ examQuest[30] = {
+question: "Linux command to convert and copy a file.",
+choicea: "A. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choiceb: "B. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choicec: "C. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;dd",
+choiced: "D. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+answer: "C"
+ };
+ 
