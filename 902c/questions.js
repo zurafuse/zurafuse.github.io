d0baa62d3bd182e8033bbc89e4e3ce8cd6950677
Timothy Horton <zurafuse@gmail.com>
2018-09-10 11:20:16 -0500

    Add files via upload

---
+++ 902c/questions.js
@@ -0,0 +1,517 @@
+var examQuest = [];
+
+examQuest[0] = {
+question: "Allows cloud storage",
+choicea: "A. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;One Drive",
+choiceb: "B. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choicec: "C. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choiced: "D. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+answer: "A"
+ };
+
+ examQuest[1] = {
+question: "A convenient location to purchase Windows apps",
+choicea: "A. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choiceb: "B. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choicec: "C. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Windows Store",
+choiced: "D. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+answer: "C"
+ };
+ 
+ examQuest[02] = {
+question: "Taskbars appear in Windows in multiple monitors",
+choicea: "A. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choiceb: "B. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Multimonitor task bars",
+choicec: "C. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choiced: "D. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+answer: "B"
+ };
+ 
+examQuest[3] = {
+question: "Commonly used apps/features such as Search and Share. Only in Windows 8.",
+choicea: "A. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Charms",
+choiceb: "B. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choicec: "C. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choiced: "D. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+answer: "A"
+ };
+ 
+ examQuest[4] = {
+question: "File Structure and paths",
+choicea: "A. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Allows orderly creation of folders and files",
+choiceb: "B. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choicec: "C. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choiced: "D. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+answer: "A"
+ };
+ 
+examQuest[5] = {
+question: "Controls changes to OS settings",
+choicea: "A. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choiceb: "B. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choicec: "C. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choiced: "D. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;User Account Control",
+answer: "D"
+ };
+ 
+examQuest[6] = {
+question: "GPRESULT switch that Saves the report in XML format at the location and with the file name specified by the <filename> parameter.",
+choicea: "A. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choiceb: "B. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choicec: "C. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;X",
+choiced: "D. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+answer: "C"
+ };
+ 
+examQuest[7] = {
+question: "GPRESULT switch that Saves the report in HTML format",
+choicea: "A. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;/H",
+choiceb: "B. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choicec: "C. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choiced: "D. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+answer: "A"
+ };
+ 
+examQuest[8] = {
+question: "GPRESULT switch that Forces Gpresult to overwrite the file name specified in the /X or /H command.",
+choicea: "A. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choiceb: "B. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choicec: "C. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;/F",
+choiced: "D. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+answer: "C"
+ };
+ 
+examQuest[9] = {
+question: "Encrypts drives. Available in each OS, but not in every edition",
+choicea: "A. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choiceb: "B. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choicec: "C. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;BitLocker",
+choiced: "D. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+answer: "C"
+ };
+ 
+examQuest[10] = {
+question: "Two possible ways to view the Control Panel",
+choicea: "A. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choiceb: "B. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Category view vs. Classic view",
+choicec: "C. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choiced: "D. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+answer: "B"
+ };
+ 
+examQuest[11] = {
+question: "The interface released with Windows 8.",
+choicea: "A. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choiceb: "B. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choicec: "C. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Metro UI",
+choiced: "D. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+answer: "C"
+ };
+ 
+examQuest[12] = {
+question: "Allows you to position windows on the desktop and Windows will remember the size and position",
+choicea: "A. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choiceb: "B. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choicec: "C. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choiced: "D. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Side by side apps",
+answer: "D"
+ };
+
+examQuest[13] = {
+question: "Another name for Metro UI",
+choicea: "A. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choiceb: "B. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choicec: "C. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choiced: "D. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Modern UI",
+answer: "D"
+ };
+ 
+examQuest[14] = {
+question: "Connecting an app to the Taskbar. Not available in Vista.",
+choicea: "A. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choiceb: "B. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Pinning",
+choicec: "C. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choiced: "D. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+answer: "B"
+ };
+ 
+examQuest[15] = {
+question: "A portion of the Metro UI from which you can choose common options",
+choicea: "A. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choiceb: "B. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Start Screen",
+choicec: "C. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choiced: "D. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+answer: "B"
+ };
+ 
+examQuest[16] = {
+question: "A scripting language that can be used to automate tasks",
+choicea: "A. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choiceb: "B. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choicec: "C. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choiced: "D. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Power Shell",
+answer: "D"
+ };
+ 
+examQuest[17] = {
+question: "A single sign on feature. Available in Windows 8 and above.",
+choicea: "A. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choiceb: "B. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choicec: "C. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Live Sign on.",
+choiced: "D. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+answer: "C"
+ };
+ 
+examQuest[18] = {
+question: "A central location for dealing with problems, security, and maintenance. Known as Security Center in Vista.",
+choicea: "A. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Action Center",
+choiceb: "B. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choicec: "C. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choiced: "D. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+answer: "A"
+ };
+ 
+examQuest[19] = {
+question: "Suggested RAM for Windows Vista Home",
+choicea: "A. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choiceb: "B. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choicec: "C. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;500 MB",
+choiced: "D. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+answer: "C"
+ };
+ 
+examQuest[20] = {
+question: "Suggested RAM for Windows Vista Premium and above",
+choicea: "A. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choiceb: "B. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;1 GB",
+choicec: "C. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choiced: "D. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+answer: "B"
+ };
+ 
+examQuest[21] = {
+question: "What are the video requirements for Windows 7 and 8?",
+choicea: "A. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choiceb: "B. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choicec: "C. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choiced: "D. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;DirectX 9 graphics device with WDDM driver",
+answer: "D"
+ };
+ 
+examQuest[22] = {
+question: "What is the Linux command to change mode of a file system object?",
+choicea: "A. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;chmod",
+choiceb: "B. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choicec: "C. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choiced: "D. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+answer: "A"
+ };
+ 
+examQuest[23] = {
+question: "Windows 7 and 8 x64 Free disk space requirement",
+choicea: "A. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choiceb: "B. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choicec: "C. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;20 GB",
+choiced: "D. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+answer: "C"
+ };
+ 
+examQuest[24] = {
+question: "CPU requirement for Windows 7",
+choicea: "A. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choiceb: "B. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choicec: "C. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;1 GHz",
+choiced: "D. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+answer: "C"
+ };
+ 
+examQuest[25] = {
+question: "CPU needed for Windows Vista Home",
+choicea: "A. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;800 MHz",
+choiceb: "B. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choicec: "C. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choiced: "D. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+answer: "A"
+ };
+ 
+ examQuest[26] = {
+question: "CPU requirement for Windows 8",
+choicea: "A. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choiceb: "B. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choicec: "C. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choiced: "D. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;1 GHz with support for PAE, NX, and SSE2",
+answer: "D"
+ };
+ 
+examQuest[27] = {
+question: "What is the max RAM that x86 Windows 8 Pro/Enterprise supports?",
+choicea: "A. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choiceb: "B. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;4 GB",
+choicec: "C. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choiced: "D. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+answer: "B"
+ };
+ 
+examQuest[28] = {
+question: "What is the max RAM does Windows 8 Pro/Enterprise x64 support?",
+choicea: "A. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choiceb: "B. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choicec: "C. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;512 GB",
+choiced: "D. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+answer: "C"
+ };
+ 
+examQuest[29] = {
+question: "Windows 7 or 8 x86 (32 bit) Free disk space requirement",
+choicea: "A. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;16 GB",
+choiceb: "B. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choicec: "C. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choiced: "D. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+answer: "A"
+ };
+ 
+examQuest[30] = {
+question: "Which Windows 8 Edition only supports max x64 RAM of 128 GB, and does not support Bitlocker, EFS, or AppLocker?",
+choicea: "A. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Core",
+choiceb: "B. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choicec: "C. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choiced: "D. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+answer: "A"
+ };
+ 
+examQuest[31] = {
+question: "CPU needed for Windows Vista Premium and above",
+choicea: "A. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choiceb: "B. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choicec: "C. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choiced: "D. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;1 GHz",
+answer: "D"
+ };
+ 
+examQuest[32] = {
+question: "What is the max x86 RAM supported by Windows Vista Business, Enterprise, and Ultimate?",
+choicea: "A. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choiceb: "B. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choicec: "C. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;4 GB",
+choiced: "D. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+answer: "C"
+ };
+ 
+examQuest[33] = {
+question: "What is the max x64 RAM supported by Windows Vista Business, Enterprise, and Ultimate?",
+choicea: "A. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choiceb: "B. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;128 GB",
+choicec: "C. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choiced: "D. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+answer: "B"
+ };
+ 
+examQuest[34] = {
+question: "Windows Vista Home Hard Drive / Free Space needed",
+choicea: "A. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choiceb: "B. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;20 GB / 15 GB",
+choicec: "C. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choiced: "D. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+answer: "B"
+ };
+ 
+examQuest[35] = {
+question: "What is the max x64 RAM supported by Windows Vista Home Premium?",
+choicea: "A. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choiceb: "B. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choicec: "C. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choiced: "D. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;16 GB",
+answer: "D"
+ };
+ 
+examQuest[36] = {
+question: "What is the max x64 RAM supported by Windows Home Basic?",
+choicea: "A. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;8 GB",
+choiceb: "B. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choicec: "C. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choiced: "D. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+answer: "A"
+ };
+ 
+ examQuest[37] = {
+question: "What is the max x64 RAM supported by Windows 7 Home premium?",
+choicea: "A. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choiceb: "B. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choicec: "C. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;16 GB",
+choiced: "D. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+answer: "C"
+ };
+ 
+examQuest[38] = {
+question: "What is the max x64 RAM supported by Windows 7 Professional, Enterprise, and Ultimate?",
+choicea: "A. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choiceb: "B. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choicec: "C. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choiced: "D. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;192 GB",
+answer: "D"
+ };
+
+examQuest[39] = {
+question: "What is the Linux command to list directory contents?",
+choicea: "A. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choiceb: "B. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choicec: "C. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;ls",
+choiced: "D. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+answer: "C"
+ };
+ 
+examQuest[40] = {
+question: "What is the Linux command to find text in a file?",
+choicea: "A. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choiceb: "B. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;grep",
+choicec: "C. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choiced: "D. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+answer: "B"
+ };
+ 
+examQuest[41] = {
+question: "Windows Vista Home PREMIUM and above Hard Drive / Free Space needed",
+choicea: "A. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;40 GB / 15 GB",
+choiceb: "B. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choicec: "C. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choiced: "D. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+answer: "A"
+ };
+ 
+examQuest[42] = {
+question: "What is the Linux command to change current directory?",
+choicea: "A. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choiceb: "B. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choicec: "C. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choiced: "D. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;cd",
+answer: "D"
+ };
+ 
+examQuest[43] = {
+question: "What is the Linux command to shut down the machine?",
+choicea: "A. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choiceb: "B. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;shutdown",
+choicec: "C. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choiced: "D. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+answer: "B"
+ };
+ 
+examQuest[44] = {
+question: "What is the Linux command to shut down the machine in 2 minutes?",
+choicea: "A. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choiceb: "B. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choicec: "C. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;sudo shutdown 2",
+choiced: "D. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+answer: "C"
+ };
+ 
+examQuest[45] = {
+question: "Windows Vista Home video RAM requirement",
+choicea: "A. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choiceb: "B. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choicec: "C. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;32 MB",
+choiced: "D. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+answer: "C"
+ };
+ 
+examQuest[46] = {
+question: "What is the Linux command to shut down the machine in 2 minutes and reboot?",
+choicea: "A. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choiceb: "B. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;sudo shutdown -r 2",
+choicec: "C. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choiced: "D. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+answer: "B"
+ };
+ 
+examQuest[47] = {
+question: "What is the Linux command to change a user password account?",
+choicea: "A. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choiceb: "B. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choicec: "C. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choiced: "D. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;passwd",
+answer: "D"
+ };
+ 
+examQuest[48] = {
+question: "What is the Linux command to Move (rename) a file?",
+choicea: "A. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;mv",
+choiceb: "B. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choicec: "C. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choiced: "D. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+answer: "A"
+ };
+ 
+examQuest[49] = {
+question: "What is the Linux command to copy a file?",
+choicea: "A. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choiceb: "B. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choicec: "C. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;cp",
+choiced: "D. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+answer: "C"
+ };
+ 
+examQuest[50] = {
+question: "Windows Vista Home PREMIUM and up video RAM requirement",
+choicea: "A. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choiceb: "B. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choicec: "C. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choiced: "D. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;128 MB",
+answer: "E"
+ };
+ 
+examQuest[51] = {
+question: "What is the Linux command to remove files or directories?",
+choicea: "A. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;rm",
+choiceb: "B. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choicec: "C. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choiced: "D. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+answer: "A"
+ };
+ 
+examQuest[52] = {
+question: "What is the Linux command to create a folder?",
+choicea: "A. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choiceb: "B. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choicec: "C. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;mkdir",
+choiced: "D. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+answer: "C"
+ };
+ 
+examQuest[53] = {
+question: "Windows 7 or 8 32 bit (x86) RAM requirement",
+choicea: "A. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choiceb: "B. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choicec: "C. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;1 GB",
+choiced: "D. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+answer: "C"
+ };
+ 
+examQuest[54] = {
+question: "What is the Linux command to change file owner and group?",
+choicea: "A. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choiceb: "B. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;chown",
+choicec: "C. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choiced: "D. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+answer: "B"
+ };
+ 
+examQuest[55] = {
+question: "What is the Linux command to view or change wireless network config?",
+choicea: "A. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;iwconfig",
+choiceb: "B. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choicec: "C. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choiced: "D. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+answer: "A"
+ };
+ 
+examQuest[56] = {
+question: "Windows 7 or 8 64 bit (x64) RAM requirement",
+choicea: "A. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choiceb: "B. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;2 GB",
+choicec: "C. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choiced: "D. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+answer: "B"
+ };
+
+
+ 
