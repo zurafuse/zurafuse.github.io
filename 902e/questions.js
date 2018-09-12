850fc0994f9b3d3479368f92d21068aeddcb999a
Timothy Horton <zurafuse@gmail.com>
2018-09-11 22:25:51 -0500

    Add files via upload

--- 902e/questions.js
+++ 902e/questions.js
@@ -1,20 +1,171 @@
 var examQuest = [];
 
 examQuest[0] = {
-question: "Linux command to view or configure an interface and IP configuration.",
-choicea: "A. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;ipconfig",
-choiceb: "B. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;apt-get",
-choicec: "C. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;ifconfig",
-choiced: "D. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;sudo",
+question: "What chkdsk switch Specifies the drive letter (followed by a colon), mount point, or volume name?",
+choicea: "A. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;/i",
+choiceb: "B. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;map",
+choicec: "C. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;volume",
+choiced: "D. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;/forceofflinefix",
 answer: "C"
  };
 
-examQuest[#] = {
-question: " ",
-choicea: "A. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
-choiceb: "B. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
-choicec: "C. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
-choiced: "D. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
-answer: "x"
+examQuest[1] = {
+question: "What SFC switch Scans integrity of all protected system files. No repair operation is performed?",
+choicea: "A. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;/VERIFYFILE",
+choiceb: "B. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;/VERIFYONLY",
+choicec: "C. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;/VERIFY",
+choiced: "D. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;/V",
+answer: "B"
  };
  
+examQuest[2] = {
+question: "What chkdsk switch Forces the volume to dismount first if necessary? All opened handles to the volume would then be invalid (implies /F)",
+choicea: "A. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;/i",
+choiceb: "B. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;/x",
+choicec: "C. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;/f",
+choiced: "D. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;/dd",
+answer: "B"
+ };
+ 
+ examQuest[3] = {
+question: "What ipconfig switch will renew the IPv4 address for the specified adapter?",
+choicea: "A. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;/release",
+choiceb: "B. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;/renew4",
+choicec: "C. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;/renew",
+choiced: "D. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;/release4",
+answer: "C"
+ };
+ examQuest[4] = {
+question: "What chkdsk switch Performs a less vigorous check of index entries? (NTFS only)",
+choicea: "A. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;/a",
+choiceb: "B. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;/l",
+choicec: "C. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;/v",
+choiced: "D. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;/i",
+answer: "D"
+ };
+ 
+ examQuest[5] = {
+question: "What chkdsk switch Must be used with '/scan', is NTFS only, and Bypasses all online repair? all defects found are queued for offline repair (i.e. 'chkdsk /spotfix').",
+choicea: "A. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;/scannow",
+choiceb: "B. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;/filename",
+choicec: "C. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;/forceoffline",
+choiced: "D. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;/forceofflinefix",
+answer: "D"
+ };
+ 
+ examQuest[6] = {
+question: "What ipconfig switch Modifies the IPv6 DHCP class id?",
+choicea: "A. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;/setclassid",
+choiceb: "B. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;/setclassid6",
+choicec: "C. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;/renew6",
+choiced: "D. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;/IPv6",
+answer: "B"
+ };
+ 
+ examQuest[7] = {
+question: "What chkdsk switch Runs an online scan on the volume? (NTFS only)",
+choicea: "A. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;/scannow",
+choiceb: "B. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;/scanos",
+choicec: "C. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;/scan",
+choiced: "D. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;/scanall",
+answer: "C"
+ };
+ 
+ examQuest[8] = {
+question: "What SFC switch Verifies the integrity of the file with full path <file>.  No repair operation is performed.",
+choicea: "A. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;/VERIFYFILE",
+choiceb: "B. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;/VERIFYONLY",
+choicec: "C. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;/VERIFY",
+choiced: "D. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;/V",
+answer: "A"
+ };
+ 
+ examQuest[9] = {
+question: "What ipconfig command will Renew the IPv6 address for the specified adapter? ",
+choicea: "A. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;/renew",
+choiceb: "B. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;/renew6",
+choicec: "C. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;/release",
+choiced: "D. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;/release6",
+answer: "B"
+ };
+ 
+ examQuest[10] = {
+question: "Which chkdsk command Specifies the files to check for fragmentation? (FAT/FAT32 only)",
+choicea: "A. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;/f",
+choiceb: "B. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;/s",
+choicec: "C. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;frag",
+choiced: "D. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;filename",
+answer: "D"
+ };
+ 
+ examQuest[11] = {
+question: "What ipconfig command Purges the DNS Resolver cache?",
+choicea: "A. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;filename",
+choiceb: "B. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;/dnsflush",
+choicec: "C. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;/flushdns",
+choiced: "D. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;/flush",
+answer: "C"
+ };
+ 
+ examQuest[12] = {
+question: "ipconfig switch that Displays all the IPv6 DHCP class IDs allowed for adapter",
+choicea: "A. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;/showclassid6",
+choiceb: "B. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;/showclassid",
+choicec: "C. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;/all",
+choiced: "D. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;/release",
+answer: "A"
+ };
+
+ examQuest[13] = {
+question: "chkdsk command that Locates bad sectors and recovers readable information (implies /F, when /scan not specified).",
+choicea: "A. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;/R",
+choiceb: "B. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;/V",
+choicec: "C. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;/C",
+choiced: "D. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;/A",
+answer: "A"
+ };
+ 
+ examQuest[14] = {
+question: "ipconfig command that Modifies the dhcp class id.",
+choicea: "A. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;/setclassid6",
+choiceb: "B. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;/setclass",
+choicec: "C. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;/setclassid",
+choiced: "D. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;/M",
+answer: "C"
+ };
+ 
+ examQuest[15] = {
+question: "chkdsk command that Re-evaluates bad clusters on the volume (implies /R) (NTFS only)",
+choicea: "A. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;/A",
+choiceb: "B. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;/E",
+choicec: "C. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;/C",
+choiced: "D. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;/B",
+answer: "D"
+ };
+ 
+ examQuest[16] = {
+question: "ipconfig command to Display the contents of the DNS Resolver Cache",
+choicea: "A. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;/displaydns",
+choiceb: "B. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;/release",
+choicec: "C. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;/all",
+choiced: "D. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;/contents",
+answer: "A"
+ };
+ 
+ examQuest[17] = {
+question: "chkdsk switch that Uses more system resources to complete a scan as fast as possible. This may have a negative performance impact on other tasks running on the system. (NTFS only, Must be used with /scan)",
+choicea: "A. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;/scannow",
+choiceb: "B. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;/registerdns",
+choicec: "C. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;/perf",
+choiced: "D. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;/scanall",
+answer: "C"
+ };
+ 
+ examQuest[18] = {
+question: "ipconfig switch that Refreshes all DHCP leases and re-registers DNS names",
+choicea: "A. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;/displaydns",
+choiceb: "B. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;/release",
+choicec: "C. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;/lease",
+choiced: "D. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;/registerdns",
+answer: "D"
+ };
