b3056bc3aface2ce288b1a42aa16ffeca4997bf9
Timothy Horton <zurafuse@gmail.com>
2018-09-11 21:49:22 -0500

    Add files via upload

---
+++ 902e/questions.js
@@ -0,0 +1,20 @@
+var examQuest = [];
+
+examQuest[0] = {
+question: "Linux command to view or configure an interface and IP configuration.",
+choicea: "A. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;ipconfig",
+choiceb: "B. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;apt-get",
+choicec: "C. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;ifconfig",
+choiced: "D. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;sudo",
+answer: "C"
+ };
+
+examQuest[#] = {
+question: " ",
+choicea: "A. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choiceb: "B. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choicec: "C. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+choiced: "D. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;text",
+answer: "x"
+ };
+ 
