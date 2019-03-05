adcb6dd63f2966ef8577d2e84d004f81fc6efbe7
Timothy Horton <zurafuse@gmail.com>
2019-03-05 14:45:18 -0600

    Add files via upload

--- js/scriptLibrary.js
+++ js/scriptLibrary.js
@@ -65,7 +65,7 @@ for (i = 0; i < $(".modBtn").length; i++){
 		{
 			modHead = "TIM HORTON'S RESUME";
 			modBody = "View my online resume to get an idea of past experience, where I have been and what I have done. References provided upon request.";
-			modFoot = "LINK: <a href='resume/index.html' target='_blank' style='color: white'>https://zurafuse.github.io/resume</a>";			
+			modFoot = "LINK: <a href='https://zurafuse.github.io/resume' target='_blank' style='color: white'>https://zurafuse.github.io/resume</a>";			
 		}
 		else if ($(this).attr("id") === "mod_fruitfrog")
 		{
@@ -77,34 +77,40 @@ for (i = 0; i < $(".modBtn").length; i++){
 		{
 			modHead = "NEIGHBORHOOD MAP OF SLAPOUT";
 			modBody = "This is a project I worked on for a class on Udacity.com. It is an interactive map of Slapout, Alabama implementing the Google Maps API. Visit the Github page at <a href='https://github.com/zurafuse/Udacity-Neighborhood-Map/' target='_blank' style='color: black'>https://github.com/zurafuse/Udacity-Neighborhood-Map</a>.";
-			modFoot = "LINK: <a href='slapout/index.html' target='_blank' style='color: white'>https://zurafuse.github.io/slapout</a>";		
+			modFoot = "LINK: <a href='https://zurafuse.github.io/slapout' target='_blank' style='color: white'>https://zurafuse.github.io/slapout</a>";		
 		}
 		else if ($(this).attr("id") === "mod_movie")
 		{
 			modHead = "Movie Trailer Website";
 			modBody = "This is a project I worked on for a class on Udacity.com. This is a movie trailer site generated with Python code. Visit the Github page at <a href='https://github.com/zurafuse/Movie-Site/' target='_blank' style='color: black'>https://github.com/zurafuse/Movie-Site</a>.";
-			modFoot = "LINK: <a href='moviesite/index.html' target='_blank' style='color: white'>www.zurafuse.com/moviesite</a>";		
+			modFoot = "LINK: <a href='https://zurafuse.github.io/moviesite' target='_blank' style='color: white'>https://zurafuse.github.io/moviesite</a>";		
 		}
 		else if ($(this).attr("id") === "mod_bible")
 		{
 			modHead = "Bible App Website";
 			modBody = "The site was created using jQuery. Each section of the Bible is a JSON object that is parsed according to your selection. This project is a Bible translation that I am working on (it will probably be years before it is complete, if ever). Visit the Github page at <a href='https://github.com/zurafuse/BibleApp' target='_blank' style='color: black'>https://github.com/zurafuse/BibleApp</a>.";
-			modFoot = "LINK: <a href='bible/index.html' target='_blank' style='color: white'>www.zurafuse.com/bible</a>";		
+			modFoot = "LINK: <a href='https://zurafuse.github.io/bible' target='_blank' style='color: white'>https://zurafuse.github.io/bible</a>";		
 		}
 		else if ($(this).attr("id") === "mod_excellent")
 		{
-			modHead = "EXCELLENT SPIRIT Behavior Application";
-			modBody = "This is a web application that I created to organize and track rewards for my children's behavior." + 
-			" This is currently only a front-end app, but my intent is for a user to create a model on the back-end" + 
-			" with Python and SQL, and use Python Flask and AJAX to pass data to the front-end. Visit the Github page at <a href='https://github.com/zurafuse/ExcellentSpirit/' target='_blank' style='color: black'>https://github.com/zurafuse/ExcellentSpirit</a>.";
-			modFoot = "LINK: <a href='excellentspirit/index.html' target='_blank' style='color: white'>www.zurafuse.com/excellentspirit</a>";		
+			modHead = "HONOR BLAST Behavior Application";
+			modBody = "This is a web application that I created as a reward system for my children's behavior. Prizes and trophies can be awarded, and experiences points and stars are earned." + 
+			" I created this application in Node.js in an interface that might remind them of a video game. Eventually, I want to turn Honor Blast into a Learning Management System (LMS)." + 
+			" Visit the Github page at <a href='https://github.com/zurafuse/ExcellentSpirit/' target='_blank' style='color: black'>https://github.com/zurafuse/HonorBlast</a>.";
+			modFoot = "DEMO: <a href='http://honorblast-env.txrk8eppcp.us-east-1.elasticbeanstalk.com' target='_blank' style='color: white'>http://honorblast-env.txrk8eppcp.us-east-1.elasticbeanstalk.com</a>";		
 		}
 		else if ($(this).attr("id") === "mod_meme")
 		{
 			modHead = "Meme Generator";
 			modBody = "This is a very simple meme generator that I created while attending the Udacity Front End Web Developer course." + 
 				" It primarily uses JavaScript to update the divs.";
-			modFoot = "LINK: <a href='meme/index.html' target='_blank' style='color: white'>www.zurafuse.com/meme</a>";		
+			modFoot = "LINK: <a href='https://zurafuse.github.io/meme/' target='_blank' style='color: white'>https://zurafuse.github.io/meme/</a>";		
+		}
+		else if ($(this).attr("id") === "mod_mathgame")
+		{
+			modHead = "Math Adventure";
+			modBody = "This is a game that I created to teach my children the Multiplication tables. I used only JavaScript to create this game. See the code for this game <a href='https://github.com/zurafuse/Math-Adventure'>Here.</a>"
+			modFoot = "LINK: <a href='https://zurafuse.github.io/mathadventure/mathgame.html' target='_blank' style='color: white'>https://zurafuse.github.io/mathadventure/mathgame.html</a>";		
 		}
 		
 		$(".mod-head").html(modHead);
